20ccf33010a9a38b9d4626010c2966257929df25
TowerLeon <top770977@163.com>
2018-09-07 00:10:28 +0800

    20180907 0010

---
+++ chrisworld-admin/src/main/java/com/example/controller/AdminController.java
@@ -0,0 +1,34 @@
+/**
+ * FileName: AdminController
+ * Author:   Lenovo
+ * Date:     9/6/2018 11:21 PM
+ * Description:
+ * History:
+ */
+package com.example.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+    @RequestMapping("index")
+    public ModelAndView page1() {
+        return new ModelAndView("/index.html") ;
+    }
+
+    @RequestMapping("map")
+    public Map<String, String>  map1() {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("name", "张三");
+        map.put("age", "28");
+        return map;
+    }
+
+}
